3746e2d71631779c61f297b8208075b6b0e6162b
snekasriDLN <snekasri777@gmail.com>
2018-05-30 00:32:57 +0530

    ControlProgramsExample

---
+++ src/org/java/ControlProgramsExample.java
@@ -0,0 +1,50 @@
+package org.java;
+
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory.Default;
+
+public class ControlProgramsExample {
+public static void main(String[] args)
+{
+	int count = 20;
+	if(count < 30) {
+		System.out.println(30);
+	}else if(count == 20)
+	{
+		System.out.println(20);
+	}
+	else
+	{
+		System.out.println(20);
+
+	}
+	
+	int response = count > 30 ? 50 : 30; 
+   int choice = 20;
+   switch (choice) {
+   case 20:
+	   System.out.println(20);
+     break;
+   case 30:
+	   System.out.println(30);
+	   break;
+	default:
+		System.out.println("default");
+		break;
+   }
+ int i = 0;
+ while (i < 10) {
+	System.out.println("i = " + i);	
+    i++;
+ }
+    int age = 25;
+    if (age >= 65)
+    System.out.println("age is greater than or equal to 65");
+    else
+    System.out.println("age is less than 65");
+ 
+}
+
+}
+
+
+
