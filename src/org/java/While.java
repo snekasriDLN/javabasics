637aefa132d7a7c0e17d10a194c543ff9dc550a6
snekasriDLN <snekasri777@gmail.com>
2018-05-30 02:14:25 +0530

    While program

---
+++ src/org/java/While.java
@@ -0,0 +1,20 @@
+package org.java;
+
+public class While {
+	public static void name(srting[] args)
+	{
+	int x = 1,total = 0;
+    while ( x <= 10 )
+    {
+    total += x;
+    ++x;
+    System.out.println(total);
+    }
+    int y=2;
+    while (y > 0)
+    {
+    System.out.println(y);
+    ++y;
+    }
+}
+}
