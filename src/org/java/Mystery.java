e91565e1cd66b005a298cf5270899749bc6952fc
snekasriDLN <snekasri777@gmail.com>
2018-05-30 02:24:17 +0530

    the program prints
    the total is 385

---
+++ src/org/java/Mystery.java
@@ -0,0 +1,24 @@
+package org.java;
+
+public class Mystery {
+	public static void main(String[] args) { 		
+		{
+		int y;
+		int x = 1;
+		int total = 0;
+
+		 while ( x <= 10 )
+		 {
+		 y = x * x;
+		 System.out.println( y );
+		 total += y;
+		 ++x;
+		 } // end while
+
+		 System.out.printf( "Total is %d\n", total );
+		 } // end main
+		 } 
+
+}
+
+
