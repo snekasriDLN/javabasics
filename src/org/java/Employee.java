a11e294cfc8178ca2d3e7e25841d012adb0cddf2
snekasriDLN <snekasri777@gmail.com>
2018-05-31 09:01:22 +0530

    Get and Set

---
+++ src/org/java/Employee.java
@@ -0,0 +1,27 @@
+package org.java;
+
+public class Employee {
+	String firstname;
+	String lastname;
+	double salary;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+
+}
