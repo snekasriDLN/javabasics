1588b61f096f449c7cbc86021a62931c57554ea9
snekasriDLN <snekasri777@gmail.com>
2018-05-31 09:09:13 +0530

    boolean

---
+++ src/org/java/Sample.java
@@ -0,0 +1,26 @@
+package org.java;
+
+public class Sample {
+public static boolean main(String args[]) {
+	Sample sample = new Sample();
+	Object system;
+	System.out.println("Is 6 a perfect number "+system.isperfect numbere+"));
+	System.out.println("Is 4 a perfect number "+system.isperfect));
+public boolean isMultiple(int one, int two) {
+	return two % one == 0 ? true : false;
+}
+public boolean isEven(int one) {
+	return one % 2 == 0 ?true :false;
+}
+public double convertCelsiusToFarenheit(double celsius))
+	return 9.0 / 5.0 * Celsius + 32;
+}
+public boolean inPerfect(int number) {
+	int sum = 0;
+	for(int i=1; i<=number/2 ; i++) {
+		if(number % i == 0) {
+			sum = sum+i;
+		}
+	}
+}
+}
