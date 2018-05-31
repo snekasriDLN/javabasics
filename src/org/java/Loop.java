82a1e3747acc60d527c7fa29de5acb2fe907e30e
snekasriDLN <snekasri777@gmail.com>
2018-05-31 09:06:21 +0530

    loops

---
+++ src/org/java/Loop.java
@@ -0,0 +1,18 @@
+package org.java;
+
+public class Loop {
+	public static void main(String[] args) {
+	int i,j,k;
+	for (i= 1; i <= 5; i++)
+	{
+	for (j= 1; j <= 3; j++)
+	{
+	for (k= 1; k <= 4; k++)
+	System.out.print('*');
+	System.out.println("");
+	} 
+	
+	System.out.println("");
+	} 
+}
+}
