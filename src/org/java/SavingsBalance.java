df61457d63632eeda930c1c389bea64916f01c09
snekasriDLN <snekasri777@gmail.com>
2018-06-01 05:24:26 +0530

    SavingsBalance

---
+++ src/org/java/SavingsBalance.java
@@ -0,0 +1,23 @@
+package org.java;
+
+public class SavingsBalance {
+	public static double annuaalInterest;
+	double savingsBalance;
+	public SavingsBalance(double balance) {
+		this.savingsBalance = balance;
+	}
+		
+		public void calculateMonthlyInterest
+		double savingsBalance = savingsBalance + interest;
+		
+	}static void calculateMonthlyInterest() {
+		double savingsBalance = (savingsBalance *(annualInterestRate/100.0))/12;
+		savingBalance = savingsBalance + interest;
+	}
+		
+	}
+	
+	
+	
+
+}
