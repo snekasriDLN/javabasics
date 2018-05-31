3fcf2a86c850ca59e1ed8e3e7e494f718a7b212d
snekasriDLN <snekasri777@gmail.com>
2018-06-01 05:27:12 +0530

    Account balance

--- src/org/java/SavingsBalance.java
+++ src/org/java/SavingsBalance.java
@@ -1,23 +1,37 @@
 package org.java;
 
 public class SavingsBalance {
-	public static double annuaalInterest;
-	double savingsBalance;
-	public SavingsBalance(double balance) {
-		this.savingsBalance = balance;
-	}
+	public static double annuaalInterestrate;
+	double SavingsAccount;
+	public SavingsBalance(double balance)
+	{
+		this.SavingsAccount = balance;
+	}	
+		public void MonthlyInterest(){
+			double Monthlyinterest = (SavingsAccount*(annuaalInterestrate/100.0))/12;
+			SavingsAccount = SavingsAccount + Monthlyinterest;
+			System.out.println("The balance is "+SavingsAccount);
+		}
+		public static void modifyInterest(int newValue) {
+			annuaalInterestrate = newValue;
+		}
+		public static void main(String args[])
+		{
+			SavingsBalance sa1 = new SavingsBalance(2000);
+			SavingsBalance sa2 = new SavingsBalance(3000);
+			SavingsBalance.modifyInterest(5);
+			sa1.MonthlyInterest();
+			sa2.MonthlyInterest();
+			SavingsBalance.modifyInterest(4);
+			sa1.MonthlyInterest();
+			sa2.MonthlyInterest();
+			
+			
 		
-		public void calculateMonthlyInterest
-		double savingsBalance = savingsBalance + interest;
+			
+			
+		}
 		
-	}static void calculateMonthlyInterest() {
-		double savingsBalance = (savingsBalance *(annualInterestRate/100.0))/12;
-		savingBalance = savingsBalance + interest;
-	}
-		
-	}
-	
-	
-	
-
 }
+		
+	
