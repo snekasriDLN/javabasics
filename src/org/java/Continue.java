f9f43dd50807abb51d1a76bbc9c217e6200fcac9
snekasriDLN <snekasri777@gmail.com>
2018-05-31 08:55:27 +0530

    Date

---
+++ src/org/java/Continue.java
@@ -0,0 +1,25 @@
+package org.java;
+public class Continue {
+
+	public static void main(String[] args) { 
+int i = 1;
+float k;
+	while ( i <= 10 );
+for(k=(float)0.1; k != 1.0;k += 0.1) {
+	continue;}
+System.out.println( k );
+
+int n = 2;
+switch (n)
+{
+case 1:
+System.out.println( "The number is 1" );
+case 2:
+System.out.println( "The number is 2" );
+break;
+default:
+System.out.println( "The number is not 1 or 2" );
+break;
+}
+}
+}
