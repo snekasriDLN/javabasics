156083de868ffe91b800cd83ea6a7453c1a12cf1
snekasriDLN <snekasri777@gmail.com>
2018-06-02 02:40:56 +0530

    Student details own program

---
+++ src/org/java/CompanyPay.java
@@ -0,0 +1,17 @@
+package org.java;
+
+public class CompanyPay {
+ 	int pay[] = new int[9];
+ 	pay[0] = a;
+ 	pay[1] = b;
+ 	pay[2] = c;
+ 	pay[3] = d;
+ 	pay[4] = e;
+ 	pay[5] = f;
+ 	pay[6] = g;
+ 	pay[7] = h;
+ 	pay[8] = i;
+ 	
+ 	
+	
+}}
