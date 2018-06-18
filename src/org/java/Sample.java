b95413fdf2167685b676d2603d604101d4f11319
snekasriDLN <snekasri777@gmail.com>
2018-06-19 04:21:52 +0530

    Sample

--- src/org/java/Sample.java
+++ src/org/java/Sample.java
@@ -1,26 +1,46 @@
 package org.java;
 
 public class Sample {
-public static boolean main(String args[]) {
-	Sample sample = new Sample();
-	Object system;
-	System.out.println("Is 6 a perfect number "+system.isperfect numbere+"));
-	System.out.println("Is 4 a perfect number "+system.isperfect));
-public boolean isMultiple(int one, int two) {
+	public boolean ismultiple(int one,int two)
+{
 	return two % one == 0 ? true : false;
 }
 public boolean isEven(int one) {
-	return one % 2 == 0 ?true :false;
+	return one % 2 == 0 ? true : false;
 }
-public double convertCelsiusToFarenheit(double celsius))
-	return 9.0 / 5.0 * Celsius + 32;
+public double celsius(int farenheit,double celsius) {
+	celsius = 5.0/9.0 * (farenheit -32);
+	return celsius;
+}
+public double farenheit(double celsius) {
+	double farenheit = 9.0 / 5.0 *(celsius + 32);
+	return farenheit;
+
 }
 public boolean inPerfect(int number) {
 	int sum = 0;
 	for(int i=1; i<=number/2 ; i++) {
-		if(number % i == 0) {
-			sum = sum+i;
-		}
+		sum = sum +((number % i == 0) ? i : 0);
 	}
+		return(number == sum) ? true : false;
+		}
+public int reverse (int numeric)
+	{
+	int value =0,i;
+	while(numeric!=0) {
+		i=numeric%10;
+		value=(value*10)+i;
+		numeric=numeric/10;
+	}
+	return value;
+	}
+public static void main(String args[]) {
+	Sample p =new Sample();
+	System.out.println("The given boolean is" +p.ismultiple(5,6));
+	System.out.println("The given boolean is" +p.isEven(9));
+	System.out.println("The given boolean is" +p.inPerfect(4));
+	System.out.println("The reverse of number is" +p.reverse(1234));
+	System.out.println("The celsius is" +p.celsius(35,45.0));
+	System.out.println("The farenheit is" +p.farenheit(65));
 }
 }
